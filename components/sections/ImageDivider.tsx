6c3d9e97a903d23f599a1b3bb0b426dfc36e0b90
Badrul Hanif <hanifbaharuddin@gmail.com>
2025-08-24 11:24:24 +0800

    fix image divider on mobile view

--- components/sections/ImageDivider.tsx
+++ components/sections/ImageDivider.tsx
@@ -2,16 +2,9 @@ import Image from "next/image";
 
 export default function ImageDividerSection() {
   return (
-    <section className="relative w-full h-[560px] sm:h-[800px] overflow-hidden">
-      <div className="absolute inset-0">
-        <Image
-          src="/Images/hero-banner-1.png"
-          alt="Gold Coast Morib International Resort Image Divider"
-          fill
-          className="object-cover"
-          priority
-        />
-      </div>
-    </section>
+    <section
+      className="w-full h-[560px] sm:h-[800px] bg-cover bg-center bg-fixed"
+      style={{ backgroundImage: "url('/Images/hero-banner-1.png')" }}
+    ></section>
   );
 }
