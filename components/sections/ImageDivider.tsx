ff60f89ced58168c2fee38f5ac94800b01fc3206
Badrul Hanif <hanifbaharuddin@gmail.com>
2025-08-24 11:12:29 +0800

    fix image divider on mobile view

--- components/sections/ImageDivider.tsx
+++ components/sections/ImageDivider.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function ImageDividerSection() {
   return (
     <section
-      className="w-full h-[560px] sm:h-[800px] bg-cover bg-center bg-fixed"
+      className="w-full h-[560px] sm:h-[800px] bg-cover bg-center bg-scroll sm:bg-fixed"
       style={{ backgroundImage: "url('/Images/hero-banner-1.png')" }}
     ></section>
   );
