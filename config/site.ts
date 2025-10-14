25793529936c1c9430dd7c40c4895e3966ac8487
citrusarc <citrusarc.studio@gmail.com>
2025-10-14 15:38:56 +0800

    update config site

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ import { ItemStatus, NavItem, FooterItem } from "@/types";
 
 export type SiteConfig = typeof siteConfig;
 
-const defaultStatus = {
+const defaultStatus: ItemStatus = {
   isDisabled: false,
   isHidden: false,
   isComingSoon: false,
@@ -46,7 +46,7 @@ export const siteConfig = {
       href: "/meetings-and-events",
       status: { ...defaultStatus },
     },
-  ] satisfies (ItemStatus | NavItem)[],
+  ] satisfies NavItem[],
   footerItems: [
     {
       id: "the-resort",
@@ -111,5 +111,5 @@ export const siteConfig = {
       href: "/privacy-policy",
       status: { ...defaultStatus },
     },
-  ],
+  ] satisfies FooterItem[],
 };
